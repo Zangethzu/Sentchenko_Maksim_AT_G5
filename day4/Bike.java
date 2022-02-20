b0c1f0e02b99fd1823c08974143f5c14d833416f
Marseille <msentchenko@gmail.com>
2022-02-20 22:20:49 +0300

    perezaliv proekta

---
+++ day4/Bike.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class Bike extends Vehicle implements Movable{
+
+    public Bike(int speed, String color) {
+        super(speed, color);
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Bike has started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Bike has stopped");
+    }
+}
