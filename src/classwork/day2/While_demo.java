697876d000bb05c721af55f5a37668acf83e2901
Marseille <msentchenko@gmail.com>
2022-02-21 22:51:28 +0300

    create new folders in repo for homework and classwork

---
+++ src/classwork/day2/While_demo.java
@@ -0,0 +1,16 @@
+package Day_2;
+
+public class While_demo {
+
+    public static void main(String[] args) {
+        int i = 23;
+        for (i = 23; i < 28; i++) {
+            if (i % 13 == 0) {
+                System.out.println("continue!");
+                break;
+            }
+            System.out.println("i is : ");
+        }
+        System.out.println("number is : " + i);
+    }
+}
