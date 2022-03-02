1a7a438119e998ee23a0036cd4e121b5480fd336
Marseille <msentchenko@gmail.com>
2022-03-02 14:21:54 +0300

    Refactor method wholeMassive.

---
+++ src/main/java/tasks/homework/massives/Runner.java
@@ -0,0 +1,11 @@
+package main.java.tasks.homework.massives;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Arrays arrays = new Arrays();
+        arrays.sumNumber(4);
+        arrays.wholeMassive(4);
+        arrays.picture();
+    }
+}
