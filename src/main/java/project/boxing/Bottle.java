8e6c9d54f110fea7fcd58c4ee15362be266324e8
Marseille <msentchenko@gmail.com>
2022-03-06 14:42:22 +0300

    refactor code

--- src/main/java/project/boxing/Bottle.java
+++ src/main/java/project/boxing/Bottle.java
@@ -7,13 +7,15 @@ public class Bottle {
 
     public double volume;
 
-  SparklingWater sparklingWater = new SparklingWater("white","clear","no",15);
+    SparklingWater sparklingWater = new SparklingWater("white", "clear", "no", 15);
 
-    public Bottle(double volume){
+    public Bottle(double volume) {
         this.volume = volume;
         this.sparklingWater.setBubbles(new Bubble[(int) (10000 * volume)]);
         this.sparklingWater.pump();
     }
 
-    public void open (){sparklingWater.degas();}
+    public void open() {
+        sparklingWater.degas();
+    }
 }
