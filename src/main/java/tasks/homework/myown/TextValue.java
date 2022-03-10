6ca780203c7f97f634e1e900f0a9d5c9f7e3a240
Marseille <msentchenko@gmail.com>
2022-03-10 14:10:58 +0300

    Created myowns works\trials

---
+++ src/main/java/tasks/homework/myown/TextValue.java
@@ -0,0 +1,16 @@
+package main.java.tasks.homework.myown;
+
+import java.util.Scanner;
+/*
+public class TextValue {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        String text = input.nextLine();
+        int indexSpace = text.indexOf(" ");
+        System.out.println(text.charAt(0) < text.charAt(indexSpace + 1));
+        input.next();
+    }
+}
+ */
