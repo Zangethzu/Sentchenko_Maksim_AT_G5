41f3eadfd529a7dda27f14718b2717e54e1a66f2
Marseille <msentchenko@gmail.com>
2022-03-13 14:16:10 +0300

    while

---
+++ src/main/java/tasks/homework/myown/while1.java
@@ -0,0 +1,19 @@
+package main.java.tasks.homework.myown;
+
+import java.util.Scanner;
+
+public class while1 {
+
+
+    public void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+        int a = 1;
+        while (value > a) {
+            System.out.print(a);
+            a++;
+
+
+        }
+    }
+}
