c05875f2051194316b251e2bc1164d6fdacb37ee
Marseille <msentchenko@gmail.com>
2022-03-15 12:59:58 +0300

    Create Cities

---
+++ src/main/java/tasks/homework/lists/MyCities.java
@@ -0,0 +1,26 @@
+package main.java.tasks.homework.lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MyCities {
+
+    public static void main(String[] args) {
+
+        List<String> cities = Arrays.asList("Минск", "Москва", "Берлин");
+        for (String city : cities) {
+            System.out.println(city);
+        }
+
+        int count = 0;
+        for (String city : cities) {
+            count += city.length();
+        }
+        System.out.println(count);
+
+        for(int city = 0; city < cities.size(); city++){
+            System.out.print(cities.get(city) + " ");
+        }
+    }
+}
