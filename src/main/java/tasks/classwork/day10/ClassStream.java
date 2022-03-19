845506165523549e24479bd16b64ab7fd40abc3c
Marseille <msentchenko@gmail.com>
2022-03-19 16:23:33 +0300

    String2 mam myla ramu

--- src/main/java/tasks/classwork/day10/ClassStream.java
+++ src/main/java/tasks/classwork/day10/ClassStream.java
@@ -8,7 +8,8 @@ public class ClassStream {
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("мама", "мыла", "раму","мама","чисто");
-        long lol = list.stream().filter(x -> x.equals("мама")).count();
+//        long lol = list.stream().filter(x -> x.equals("мама")).count(); Более простой вариант того что написано ниже
+        long lol = list.stream().filter("мама"::equals).count();
         System.out.println(lol);
         list.forEach(System.out::println);
 
