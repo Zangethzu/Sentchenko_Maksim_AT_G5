b89fcd2d65178996ceec429138baf7acb7daf399
Marseille <msentchenko@gmail.com>
2022-03-19 15:56:40 +0300

    String mam myla ramu

---
+++ src/main/java/tasks/classwork/day10/ClassStream.java
@@ -0,0 +1,16 @@
+package main.java.tasks.classwork.day10;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ClassStream {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("мама", "мыла", "раму","мама","чисто");
+        long lol = list.stream().filter(x -> x.equals("мама")).count();
+        System.out.println(lol);
+        list.forEach(System.out::println);
+
+    }
+}
