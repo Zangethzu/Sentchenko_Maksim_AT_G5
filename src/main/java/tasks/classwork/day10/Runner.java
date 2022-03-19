800df9da68dda164e7f51791d180e6e24ada550e
Marseille <msentchenko@gmail.com>
2022-03-19 15:28:36 +0300

    Lambdas

---
+++ src/main/java/tasks/classwork/day10/Runner.java
@@ -0,0 +1,9 @@
+package main.java.tasks.classwork.day10;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        MyClass mc = new MyClass();
+        mc.justMethod(ClassX::new);
+    }
+}
---
+++ src/main/java/tasks/homework/lists/MyBubbles.java
@@ -0,0 +1,38 @@
+package main.java.tasks.homework.lists;
+
+import main.java.project.internal.Bubble;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//public class MyBubbles {
+//    public static void main(String[] args) {
+//
+//        public class MyBubbles {
+//            public static <Bubblee> void main(String[] args) {
+//
+//                List<Bubble> bubbles = new ArrayList<>(Arrays.asList(new NewBubble(2, "CO2"), new NewBubble(4, "O2"), new NewBubble(5, "CO")));
+//                int volumes = 0;
+//
+//
+//                for (Bubble bubble : bubbles) {
+//                    System.out.print(bubble.getVolume() + " ");
+//                    volumes += bubble.getVolume();
+//                }
+//
+//                System.out.println();
+//
+//                for (Bubble bubble : bubbles) {
+//                    System.out.print(bubble.getName() + " ");
+//                }
+//
+//                System.out.println("\n" + volumes);
+//
+//                for (int i = 0; i < bubbles.size(); i++) {
+//                    System.out.println(bubbles.get(i).toString());
+//                }
+//            }
+//        }
+//    }
+//}
