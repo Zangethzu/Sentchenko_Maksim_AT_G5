f3c97959d05a885664aef01a26533e11ee43d70f
Marseille <msentchenko@gmail.com>
2022-03-20 15:00:08 +0300

    Create Class Stream3

---
+++ src/main/java/tasks/classwork/day10/ClassStream3.java
@@ -0,0 +1,19 @@
+package main.java.tasks.classwork.day10;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClassStream3 {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("мама", "мыла", "раму","мама");
+
+        System.out.println(list.stream().anyMatch("мама"::equals));
+        System.out.println(list.stream().allMatch("м"::contains));
+        System.out.println(list.stream().map(x -> x +"м").collect(Collectors.toList()));
+        list.stream().map(x -> x +"м" + " ").forEach(System.out::println);
+        System.out.println(list.stream().flatMap(x -> Arrays.stream(x.split("a"))).filter(x -> !x.equals("")).collect(Collectors.toList()));
+    }
+}
