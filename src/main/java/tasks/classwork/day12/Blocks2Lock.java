61fcc5a70e0cec1321a1fbc92ccaf96c629433f8
Zangethzu <metall02.94@gmail.com>
2022-03-27 16:31:46 +0300

    Created BlocksLock

---
+++ src/main/java/tasks/classwork/day12/Blocks2Lock.java
@@ -0,0 +1,32 @@
+package main.java.tasks.classwork.day12;
+
+public class Blocks2Lock {
+    public static void main(String[] args) throws InterruptedException {
+
+        Object lock = new Object();
+
+        new Thread(() -> {
+            synchronized (lock) {
+                for (int i = 0; i < 3; i++) {
+                    try {
+                        Thread.currentThread().sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.printf("t1-%s", i);
+                }
+            }
+        }).start();
+
+        Thread.sleep(1000);
+        
+        synchronized (lock) {
+            {
+                for (int i = 0; i < 3; i++) {
+                    Thread.currentThread().sleep(1000);
+                    System.out.printf("m-%s ", i);
+                }
+            }
+        }
+    }
+}
