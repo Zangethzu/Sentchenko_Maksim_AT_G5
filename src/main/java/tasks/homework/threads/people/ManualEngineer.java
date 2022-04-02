605dd55fac200460301cf6ec82148fcb734c58c1
Zangethzu <metall02.94@gmail.com>
2022-04-02 20:22:22 +0300

    Created ManualEngineer

---
+++ src/main/java/tasks/homework/threads/people/ManualEngineer.java
@@ -0,0 +1,9 @@
+package main.java.tasks.homework.threads.people
+
+public class ManualEngineer extends Engineer {
+
+    public ManualEngineer(int age, int skill, int experience) {
+        super(age, experience * 2, experience);
+    }
+
+}
