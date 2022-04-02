df40e752e688be1eb29141f76b93171e6cf3f737
Zangethzu <metall02.94@gmail.com>
2022-04-02 20:21:13 +0300

    Created AutomatedEngineer

---
+++ src/main/java/tasks/homework/threads/people/AutomatedEngineer.java
@@ -0,0 +1,9 @@
+package main.java.tasks.homework.threads.people;
+
+public class AutomatedEngineer extends Engineer {
+
+    public AutomatedEngineer(int age, int experience) {
+        super(age, experience * 3, experience);
+    }
+
+}
