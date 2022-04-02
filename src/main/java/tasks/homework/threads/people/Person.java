0f071d20fcb4b12f15a68cf883160969b5bdd82d
Zangethzu <metall02.94@gmail.com>
2022-04-02 20:21:55 +0300

    Created Person

---
+++ src/main/java/tasks/homework/threads/people/Person.java
@@ -0,0 +1,19 @@
+package main.java.tasks.homework.threads.people;
+
+public class Person {
+
+    int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
